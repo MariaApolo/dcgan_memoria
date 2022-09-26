1c9707f35724ea8608c67e2daa0d323cf2c60c46
practicaventures <practicaventures@digevo.com>
2022-09-26 17:45:24 -0300

    config, dataloader

--- configs/config.py
+++ configs/config.py
@@ -1,36 +1,33 @@
-# Root directory for dataset
-dataroot = '/content/drive/Shared drives/SpotifyMemoria/MM_full/Knn/'
+# -*- coding: utf-8 -*-
+"""Model config in json format"""
 
-# Number of workers for dataloader
-workers = 2
+CFG = {
+    "data": {
+        "dataroot":'/content/drive/Shared drives/SpotifyMemoria/MM_full/Knn/', #data folder
+        "image_size": 64, #all images will be resized to this
+        "workers": 2, #num of workers for dataloader
+    },
+    "train": {
+        "batch_size": 128, #batch size during training
+        "nc": 3, #Number of channels in the training images
+        "nz": 100, # Size of z latent vector (i.e. size of generator input)
+        "ngf": 64, # Size of feature maps in generator
+        "ndf": 64, # Size of feature maps in discriminator
+        #"buffer_size": 1000 #to shuffle images
+        "epoches": 1000,
+        "lr": 0.0002,
 
-# Batch size during training
-batch_size = 128
+        #"val_subsplits": 5,
+        "optimizer": {
+            "type": "adam",
+            "beta1": 0.5 # Beta1 hyperparam for Adam optimizers
+        },
+        "metrics": ["accuracy"],
+        "ngpu": 1
+    },
+    "model": {
+        "input": [128, 128, 3],
 
-# Spatial size of training images. All images will be resized to this
-#   size using a transformer.
-image_size = 64
-
-# Number of channels in the training images. For color images this is 3
-nc = 3
-
-# Size of z latent vector (i.e. size of generator input)
-nz = 100
-
-# Size of feature maps in generator
-ngf = 64
-
-# Size of feature maps in discriminator
-ndf = 64
-
-# Number of training epochs
-num_epochs = 1000
-
-# Learning rate for optimizers
-lr = 0.0002
-
-# Beta1 hyperparam for Adam optimizers
-beta1 = 0.5
-
-# Number of GPUs available. Use 0 for CPU mode.
-ngpu = 1
+        "output": 3
+    }
+}
